67c8ad604832e10583d43e5657687608ef9ae7ec
stanleycliu003 <stanleyliu003@gmail.com>
2026-01-03 12:14:09 -0800

    Created getFlightInfo lambda function and implementation plan for add flights feature

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -73,6 +73,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "getFlightInfo": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "getLocationCoordinates": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
